66bbc18c9375818f5c1b662016513b6e7f668b1d
Starlk <155927834+Starlk7@users.noreply.github.com>
2024-01-08 21:44:16 +0100

    Add files via upload

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 module.exports = {
-    "threads": 1, //1-3
+    "threads": 1,
     "link":"https://magiceden.io/launchpad/phoenix",
     "seedPhrases": ["grid popular winner turkey cushion violin guide actor milk evoke catch very",
                   "snow across blanket ski holiday steel height feel spread high strike pepper"],
-    "headless": 0 //1 - true | 0 - false
+    "headless": false
 };

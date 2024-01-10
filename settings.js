561ea43f3ba35dd0afebfaedf6a477a566c514cd
Starlk <155927834+Starlk7@users.noreply.github.com>
2024-01-10 22:02:29 +0100

    BTC support

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 module.exports = {
     "threads": 1,
-    "link":"https://magiceden.io/launchpad/flash_node",
+    "link":"https://magiceden.io/ordinals/launchpad/pizza-ninjas",
     "seedPhrases": ["grid popular winner turkey cushion violin guide actor milk evoke catch very"],
     "headless": false
 };
